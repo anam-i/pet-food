d5dc389a38b0f82696ad8f2869ae516d55a2f758
anam-i <inaam.anam@gmail.com>
2025-05-05 17:42:13 -0600

    Update petfood.py

--- petfood.py
+++ petfood.py
@@ -166,7 +166,7 @@ if submit:
     # Filter rows from the CSV where Product_id matches
 recommended_products = df_productdata[df_productdata['Product_ID'].isin(product_ids)]
     # Display the filtered products
-st.write(f"Recommended Products: {product_ids}")
+# st.write(f"Recommended Products: {product_ids}")
 	# Display the product names in Streamlit
 st.write("### Recommended Products:")
 for _, row in recommended_products.iterrows():

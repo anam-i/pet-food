232e1125b3f6f7419cc634f820930386bf9662cc
anam-i <inaam.anam@gmail.com>
2025-05-05 16:48:43 -0600

    Update rule_based_script__v3.py

--- rule_based_script__v3.py
+++ rule_based_script__v3.py
@@ -502,7 +502,7 @@ def filter_products(df_pet_info, df_products):
     df_filtered = filter_by_condition(df_products, species_col, 1)
 
     # Filter by main issue (if any)
-    main_issue = df_pet_info['main issue'].strip()
+    # main_issue = df_pet_info['main issue'].strip()
     if main_issue and main_issue in disease_product_mapping:
         disease_info = disease_product_mapping[main_issue]
         #print("disease_info",disease_info)

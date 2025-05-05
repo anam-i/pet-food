35fbe059d5de8a43619ff7c7df7eb412376fea3f
anam-i <inaam.anam@gmail.com>
2025-05-05 17:08:13 -0600

    Update rule_based_script__v3.py

--- rule_based_script__v3.py
+++ rule_based_script__v3.py
@@ -516,7 +516,7 @@ def filter_products(df_pet_info, df_products):
         #print("rows after category:",len(df_filtered))
 
     # Filter by allergies
-    if df_pet_info['has_allergy'] == 1:
+    if df_pet_info.iloc[0]['has_allergy'] == 1:
         for ingredient in df_pet_info['selected_allergies']:
             if ingredient == 'unknown':
               filter_by_condition(df_filtered, 'category_non-allergenic', 1)

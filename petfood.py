e348cba53219ddb6daaf9c972ef206efe7de341c
anam-i <inaam.anam@gmail.com>
2025-05-06 13:53:21 -0600

    Update petfood.py

--- petfood.py
+++ petfood.py
@@ -153,7 +153,7 @@ recommended_products = df_productdata[df_productdata['Product_ID'].isin(product_
     # Display the filtered products
 st.write(f"Recommended Products: {product_ids}")
 	# Display the product names in Streamlit
-st.write("### Recommended Products:")
+# st.write("### Recommended Products:")
 for _, row in recommended_products.iterrows():
 	st.write(f"- {row['Product_Name']} (ID: {row['Product_ID']})")
 

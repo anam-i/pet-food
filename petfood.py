b91f3e6a23ba02ca8f8daff65de7e0640ee8b68a
anam-i <inaam.anam@gmail.com>
2025-05-06 14:21:05 -0600

    Update petfood.py

--- petfood.py
+++ petfood.py
@@ -53,7 +53,7 @@ life_stages = ["Growth", "Adult", "Senior"]
 # ------------------ Streamlit UI ------------------
 
 st.set_page_config(page_title="Pet Nutrition Recommender", layout="centered")
-st.title("🐾🐶Pet Food Recommendation Tool Rule-Based Recommendations🐱🐾")
+st.title("🐾🐶Pet Food Recommendation Tool - Rule-Based Recommendations🐱🐾")
 
 # Gender selector
 has_gender = st.radio("Gender", options=["Male", "Female"], index=None)
